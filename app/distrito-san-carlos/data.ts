0dd5a05214905e545398c256ebd970c63460df98
webancosur-oss <web.ancosur@gmail.com>
2026-07-30 17:03:50 -0500

    Update brochure path for Distrito San Carlos and add new Neo Origen brochure

--- app/distrito-san-carlos/data.ts
+++ app/distrito-san-carlos/data.ts
@@ -2,7 +2,7 @@ export const whatsappDistritoSanCarlos =
   "https://wa.me/51971069763?text=Hola%2C%20vengo%20de%20la%20web%20de%20ANCOSUR%20y%20quiero%20informaci%C3%B3n%20sobre%20Distrito%20San%20Carlos";
 
 export const brochureDistritoSanCarlos =
-  "/assets/projects/distrito-san-carlos/brochure/brochure-distrito-san-carlos.pdf";
+  "/assets/docs/distrito-san-carlos-brochure.pdf";
 
 export const hero = {
   logo: "/assets/images/distrito-sancarlos.svg",
